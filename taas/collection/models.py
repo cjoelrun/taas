c88f09366a365f1a2716c42441d530268edbdaae
AnObfuscator <anobfuscator@gmail.com>
2017-08-06 14:05:54 -0500

    Remove obsolete collections package

--- taas/collection/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import relationship
-
-from taas.database import Model, db
-
-
-class Collection(Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    test_cases = relationship("TestCase")
