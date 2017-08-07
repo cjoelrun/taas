d11c45d049ed51fb5de050e61bd2f765cbf97635
AnObfuscator <anobfuscator@gmail.com>
2017-08-06 19:18:51 -0500

    Add CRUD methods and serialization for Executions

--- taas/execution/models.py
+++ taas/execution/models.py
@@ -12,3 +12,21 @@ class Execution(Model):
     data = db.Column(JSONB)
 
     steps = db.relationship("Step", backref="execution")
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'description': self.description,
+            'type': self.type,
+            'strategy': self.strategy,
+            'data': self.data,
+            'steps': self.serialize_steps()
+        }
+
+    def serialize_steps(self):
+        """
+       Return object's relations in easily serializeable format.
+       NB! Calls many2many's serialize property.
+       """
+        # return [item.serialize() for item in self.many2many]
+        return []
--- taas/execution/views.py
+++ taas/execution/views.py
@@ -1,10 +1,48 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
+from taas.database import db
 from taas.execution.models import Execution
 
 blueprint = Blueprint('execution', __name__, url_prefix='/executions')
 
 
-@blueprint.route('/')
+@blueprint.route('', methods=['GET', 'POST'])
 def executions():
-    return jsonify(Execution.query.all())
+    if request.method == 'GET':
+        return jsonify([e.serialize() for e in Execution.query.all()])
+
+    if request.method == 'POST':
+        execution = Execution()
+        _set_fields(execution, request.json)
+        db.session.add(execution)
+        db.session.commit()
+
+        return jsonify(execution.serialize()), 201
+
+
+@blueprint.route('/<db_id>', methods=['GET', 'PUT', 'DELETE'])
+def executions(db_id):
+    execution = Execution.query.get(db_id)
+
+    if request.method == 'GET':
+        if execution is None:
+            return '{} not found.'.format(db_id), 404
+        return jsonify(execution.serialize())
+
+    if request.method == 'PUT':
+        _set_fields(execution, request.json)
+        db.session.commit()
+        return jsonify(execution.serialize()), 200
+
+    if request.method == 'DELETE':
+        if execution is not None:
+            db.session.delete(execution)
+            db.session.commit()
+        return '', 204
+
+
+def _set_fields(execution, json):
+    execution.data = json.get('data', None)
+    execution.description = json.get('description', None)
+    execution.strategy = json.get('strategy', None)
+    execution.type = json.get('type', None)
