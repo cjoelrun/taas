3d0fd58024c8d5994af90cf245f2976daffeb526
AnObfuscator <anobfuscator@gmail.com>
2017-08-06 22:52:49 -0500

    Add CRUD methods and serialization for TestSuiteRuns

--- taas/test_suite_run/views.py
+++ taas/test_suite_run/views.py
@@ -1,9 +1,35 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from taas.test_suite_run.models import TestSuiteRun
+from taas.database import db
 
 blueprint = Blueprint('test_suite_run', __name__, url_prefix='/test_suite_runs')
 
 
-@blueprint.route('/')
+@blueprint.route('', methods=['GET', 'POST'])
 def test_suite_runs():
-    return jsonify(TestSuiteRun.query.all())
+    if request.method == 'GET':
+        return jsonify([e.serialize() for e in TestSuiteRun.query.all()])
+
+    if request.method == 'POST':
+        test_suite_run = TestSuiteRun()
+        test_suite_run.update_fields(request.json)
+        db.session.add(test_suite_run)
+        db.session.commit()
+
+        return jsonify(test_suite_run.serialize()), 201
+
+
+@blueprint.route('/<db_id>', methods=['GET', 'PUT', 'DELETE'])
+def test_suite_runs(db_id):
+    test_suite_run = TestSuiteRun.query.get(db_id)
+
+    if request.method == 'GET':
+        if test_suite_run is None:
+            return '{} not found.'.format(db_id), 404
+        return jsonify(test_suite_run.serialize())
+
+    if request.method == 'DELETE':
+        if test_suite_run is not None:
+            db.session.delete(test_suite_run)
+            db.session.commit()
+        return '', 204
