a95938bcb400b81373a07dca1d8914d66c8f868c
AnObfuscator <anobfuscator@gmail.com>
2017-08-06 22:46:00 -0500

    Add CRUD methods and serialization for TestSuites

--- taas/test_suite/models.py
+++ taas/test_suite/models.py
@@ -16,4 +16,23 @@ class TestSuite(Model):
 
     test_suite_runs = db.relationship("TestSuiteRun", backref='test_suite', lazy='dynamic')
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'short_name': self.short_name,
+            'name': self.name,
+            'description': self.description,
+            'test_cases': self.serialize_test_cases(),
+            'test_suite_runs': self.serialize_test_suite_runs()
+        }
 
+    def serialize_test_cases(self):
+        return [item.serialize() for item in self.test_cases]
+
+    def serialize_test_suite_runs(self):
+        return [item.serialize() for item in self.test_suite_runs]
+
+    def update_fields(self, json):
+        self.short_name = json.get('short_name', None)
+        self.name = json.get('name', None)
+        self.description = json.get('description', None)
