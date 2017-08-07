f3073945e7b6beccc4b3c0cd463ee393e0f773a0
AnObfuscator <anobfuscator@gmail.com>
2017-08-06 21:50:06 -0500

    Add CRUD methods and serialization for TestCases

--- taas/test_case/models.py
+++ taas/test_case/models.py
@@ -15,9 +15,37 @@ class TestCase(Model):
     description = db.Column(db.String)
     expected_success = db.Column(db.Boolean, default=True)
 
-    collection_id = db.Column(db.Integer, db.ForeignKey('collection.id'))
+    suite_id = db.Column(db.Integer, db.ForeignKey('suite.id'))
 
     test_runs = db.relationship("TestRun", backref='test_case', lazy='dynamic')
     steps = db.relationship("Step", backref='test_cases', lazy='dynamic')
     parameters = db.relationship("Parameter", secondary=test_parameters, backref=db.backref('test_cases', lazy='dynamic'))
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'short_name': self.short_name,
+            'name': self.name,
+            'description': self.description,
+            'expected_success': self.expected_success,
+            'suite_id': self.suite_id,
+            'test_runs': self.serialize_test_runs(),
+            'steps': self.serialize_steps(),
+            'parameters': self.serialize_parameters()
+        }
+
+    def serialize_test_runs(self):
+        return [item.serialize() for item in self.test_runs]
+
+    def serialize_steps(self):
+        return [item.serialize() for item in self.steps]
+
+    def serialize_parameters(self):
+        return [item.serialize() for item in self.parameters]
+
+    def update_fields(self, json):
+        self.short_name = json.get('short_name', None)
+        self.name = json.get('name', None)
+        self.description = json.get('description', None)
+        self.expected_success = json.get('expected_success', None)
+        self.suite_id = json.get('suite_id', None)
--- taas/test_case/views.py
+++ taas/test_case/views.py
@@ -1,10 +1,41 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 from taas.test_case.models import TestCase
+from taas.database import db
 
 blueprint = Blueprint('test_case', __name__, url_prefix='/test_cases')
 
 
-@blueprint.route('/')
+@blueprint.route('', methods=['GET', 'POST'])
 def test_cases():
-    return jsonify(TestCase.query.all())
+    if request.method == 'GET':
+        return jsonify([tc.serialize() for tc in TestCase.query.all()])
+
+    if request.method == 'POST':
+        test_case = TestCase()
+        test_case.update_fields(request.json)
+        db.session.add(test_case)
+        db.session.commit()
+
+        return jsonify(test_case.serialize()), 201
+
+
+@blueprint.route('/<db_id>', methods=['GET', 'PUT', 'DELETE'])
+def test_cases(db_id):
+    test_case = TestCase.query.get(db_id)
+
+    if request.method == 'GET':
+        if test_case is None:
+            return '{} not found.'.format(db_id), 404
+        return jsonify(test_case.serialize())
+
+    if request.method == 'PUT':
+        test_case.update_fields(request.json)
+        db.session.commit()
+        return jsonify(test_case.serialize()), 200
+
+    if request.method == 'DELETE':
+        if test_case is not None:
+            db.session.delete(test_case)
+            db.session.commit()
+        return '', 204
