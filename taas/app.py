8a7f2e0b4e96d1e513323859b7c95b0b0b3fda51
AnObfuscator <anobfuscator@gmail.com>
2017-08-11 17:21:34 -0500

    Create DB if it doesn't exist

--- taas/app.py
+++ taas/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from sqlalchemy_utils import database_exists, create_database
 
 from taas import test_case, execution, execution_run, \
     parameter, step, test_run, test_suite, test_suite_run
@@ -11,6 +12,7 @@ def create_app(config=DevConfig):
     app = Flask(__name__)
     app.config.from_object(config)
     db.init_app(app)
+    create_db(config)
     db.create_all(app=app)
     ma.init_app(app)
     register_blueprints(app)
@@ -27,5 +29,10 @@ def register_blueprints(app):
     app.register_blueprint(test_run.views.blueprint)
     app.register_blueprint(test_suite.views.blueprint)
     app.register_blueprint(test_suite_run.views.blueprint)
+
+
+def create_db(config):
+    if not database_exists(config.SQLALCHEMY_DATABASE_URI):
+        create_database(config.SQLALCHEMY_DATABASE_URI)
 
 app = create_app()
