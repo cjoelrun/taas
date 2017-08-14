83e77d793b39adee7114dd612109f69273827c39
AnObfuscator <anobfuscator@gmail.com>
2017-08-14 11:07:10 -0500

    Add index

--- taas/execution_run/models.py
+++ taas/execution_run/models.py
@@ -3,8 +3,9 @@ from taas.serialization import Schema
 
 
 class ExecutionRun(Model):
-    test_run_id = db.Column(db.Integer, db.ForeignKey('test_run.id'), primary_key=True)
-    step_id = db.Column(db.Integer, db.ForeignKey('step.id'), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    test_run_id = db.Column(db.Integer, db.ForeignKey('test_run.id'))  # , primary_key=True)
+    step_id = db.Column(db.Integer, db.ForeignKey('step.id'))  # , primary_key=True)
     message = db.Column(db.String)
     context = db.Column(db.String)
     status = db.Column(db.String)
