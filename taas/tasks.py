8429260f8c20b6021fce5fd298961ccb7885318a
AnObfuscator <anobfuscator@gmail.com>
2017-08-19 20:51:50 -0500

    Refactor celery task and clean up task usage

---
+++ taas/tasks.py
@@ -0,0 +1,20 @@
+from taas.async import celery
+from taas.database import db
+from taas.execution_run.models import ExecutionRun
+import requests
+
+
+@celery.task
+def run_strategy(execution_run_id, strategy_name, parameters):
+    session = db.create_scoped_session()
+    execution_run = session.query(ExecutionRun).get(execution_run_id)
+    execution_run.status = 'Running'
+    session.commit()
+
+    print('Running strategy {} with {}'.format(strategy_name, parameters))
+
+    execution_run.status = 'Success'
+    db.session.commit()
+
+    response = requests.post('{}/run/execution-runs/{}/finish'.format(celery.callback_url, execution_run_id))
+    print(response)
