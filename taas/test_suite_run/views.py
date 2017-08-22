860037647fea329e736d6b7654a2a8918a690227
AnObfuscator <anobfuscator@gmail.com>
2017-08-21 20:47:53 -0500

    Add PUT methods

--- taas/test_suite_run/views.py
+++ taas/test_suite_run/views.py
@@ -14,19 +14,24 @@ def test_suite_runs():
         return test_suite_run_schema.dumps(all_test_runs, many=True)
 
 
-@blueprint.route('/<db_id>', methods=['GET', 'DELETE'])
+@blueprint.route('/<db_id>', methods=['GET', 'PUT', 'DELETE'])
 def test_suite_runs_by_id(db_id):
     from taas.test_suite_run.schemas import test_suite_run_schema
 
-    test_run = TestSuiteRun.query.get(db_id)
+    test_suite_run = TestSuiteRun.query.get(db_id)
 
     if request.method == 'GET':
-        if test_run is None:
+        if test_suite_run is None:
             return '{} not found.'.format(db_id), 404
-        return test_suite_run_schema.dumps(test_run).data
+        return test_suite_run_schema.dumps(test_suite_run).data
+
+    if request.method == 'PUT':
+        test_suite_run_schema.load(request.json, db.session, test_suite_run)
+        db.session.commit()
+        return test_suite_run_schema.dumps(test_suite_run).data, 200
 
     if request.method == 'DELETE':
-        if test_run is not None:
-            db.session.delete(test_run)
+        if test_suite_run is not None:
+            db.session.delete(test_suite_run)
             db.session.commit()
         return '', 204
