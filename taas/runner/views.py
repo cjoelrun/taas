a979955402d851bc3853c80d5b7f88e2d6789b45
AnObfuscator <anobfuscator@gmail.com>
2017-11-09 23:07:47 -0600

    Add Parameter & ParameterGroup validation

--- taas/runner/views.py
+++ taas/runner/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 from taas.database import db
+from taas.parameter.models import Parameter
 from taas.runner.service import create_test_case_run, create_test_suite_run, execute_test_case_run, execute_test_suite_run
 from taas.test_case.models import TestCase
 from taas.test_suite.models import TestSuite
@@ -11,8 +12,15 @@ blueprint = Blueprint('runner', __name__, url_prefix='/run')
 @blueprint.route('/test-cases/<db_id>', methods=['POST'])
 def run_test_case(db_id):
     from taas.test_run.schemas import test_run_schema
+    parameter_id = request.json['parameter_id']
+    parameter = Parameter.query.get(parameter_id)
     test_case = TestCase.query.get(db_id)
-    test_run = create_test_case_run(test_case, request.json['parameter_id'])
+
+    if parameter.parameter_group_id != test_case.parameter_group_id:
+        error_message = 'Parameter {} not a member of ParameterGroup {}'.format(parameter_id, test_case.parameter_group_id)
+        return {'error': error_message}, 422
+
+    test_run = create_test_case_run(test_case, parameter_id)
     execute_test_case_run(test_run)
     return test_run_schema.dumps(test_run).data, 200
 
@@ -20,8 +28,15 @@ def run_test_case(db_id):
 @blueprint.route('/test-suites/<db_id>', methods=['POST'])
 def run_test_suite(db_id):
     from taas.test_suite_run.schemas import test_suite_run_schema
+    parameter_id = request.json['parameter_id']
+    parameter = Parameter.query.get(parameter_id)
     test_suite = TestSuite.query.get(db_id)
-    test_suite_run = create_test_suite_run(test_suite, request.json['parameter_id'])
+
+    if parameter.parameter_group_id != test_suite.parameter_group_id:
+        error_message = 'Parameter {} not a member of ParameterGroup {}'.format(parameter_id, test_suite.parameter_group_id)
+        return {'error': error_message}, 422
+
+    test_suite_run = create_test_suite_run(test_suite, parameter_id)
     execute_test_suite_run(test_suite_run)
     return test_suite_run_schema.dumps(test_suite_run).data, 200
 
