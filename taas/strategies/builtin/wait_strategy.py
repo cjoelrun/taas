9e44c46cd105312b3e8af5ba9908260f7aa8ecff
AnObfuscator <anobfuscator@gmail.com>
2017-11-09 23:08:29 -0600

    Simplify strategy interface API

--- taas/strategies/builtin/wait_strategy.py
+++ taas/strategies/builtin/wait_strategy.py
@@ -4,10 +4,8 @@ from taas.strategies.base_strategy import BaseStrategy, Result
 
 
 class WaitStrategy(BaseStrategy):
-    def perform_action(self):
+    def execute(self):
         wait_time = self._execution_data.get('wait_time', 1)
         time.sleep(wait_time)
         print("Waited {} seconds".format(wait_time))
-
-    def perform_validation(self):
         return Result(True, "Wait complete.")
