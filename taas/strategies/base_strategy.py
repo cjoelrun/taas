1e59c3a6c8fcf827ea697282f8555ac04f6f4020
AnObfuscator <anobfuscator@gmail.com>
2017-11-12 14:34:25 -0600

    Improve parameter usage and support runtime-only ephemeral parameters for sharing data between executions

--- taas/strategies/base_strategy.py
+++ taas/strategies/base_strategy.py
@@ -1,14 +1,21 @@
 
 class BaseStrategy(object):
-    def __init__(self, parameters=None, execution_data=None):
+    def __init__(self, parameters, execution_data, runtime_data):
         self._parameters = parameters
         self._execution_data = execution_data
+        self._runtime_data = runtime_data
 
     def execute(self):
         raise NotImplementedError
 
+    def add_runtime_entry(self, key, value):
+        self._runtime_data[key] = value
+
+    def get_runtime_data(self):
+        return self._runtime_data
+
 
 class Result:
-    def __init__(self, success=None, message=None):
+    def __init__(self, success, message):
         self.success = success
         self.message = message
